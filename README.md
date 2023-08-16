4e2bd24dc88b07d080797f31825fe978686e1e1e
Ezenwa Hopekell <46373141+HopekellDev@users.noreply.github.com>
2023-08-16 21:47:30 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,57 @@
+# Duplo 👑👑 (Laravel 10 Package)
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Total Downloads][ico-downloads]][link-downloads]
+
+
+> Implement Duplo B2B payment gateway easily with Laravel
+
+- Go to [Duplo](https://app.tryduplo.com/app/settings) to get your secrete key and business id
+
+
+## Documentation
+
+ A friendly documentation can be found [here](#)
+
+
+## Credits
+
+- [Hope Chukwuemeka (Hopekell)][link-author]
+
+## Contributing
+Please feel free to fork this package and contribute by submitting a pull request to enhance the functionalities. I will appreciate that a lot. Also please add your name to the credits.
+
+Kindly [follow me on twitter](https://twitter.com/hopekelltech)!
+
+## Features
+
+The current features have been implemented
+
+- Virtual Account
+- Digital Wallet
+- Transfers
+- Banks
+
+I will be working on this next
+- Invoice
+- Beneficiaries
+- Checkout
+
+> If there are features you need urgently, I will be willing to prioritize them, please reach out to my twitter account
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/hopekelldev/laravel-duplo.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/hopekelldev/laravel-duplo.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/hopekelldev/laravel-duplo
+[link-travis]: https://travis-ci.org/toondaey/laravelrave
+[link-scrutinizer]: https://scrutinizer-ci.com/g/hopekelldev/laravel-duplo/code-structure
+[link-code-quality]: https://scrutinizer-ci.com/g/HopekellDev/LaravelDuplo/
+[link-downloads]: https://packagist.org/packages/hopekelldev/laravel-duplo
+[link-author]: https://github.com/hopekelldev
+[link-coverage]: https://scrutinizer-ci.com/g/HopekellDev/LaravelDuplo/
+[link-code-intelligence]: https://scrutinizer-ci.com/code-intelligence
--- src/Helpers/Payment.php
+++ src/Helpers/Payment.php
@@ -52,4 +52,111 @@ class Payment
                 
         return $otp;
     }
+
+    /**
+     * Get wallet balance
+     */
+    public function getBalance($wallet_ref)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->apiKey,
+            'Content-Type' => 'application/json'
+        ];
+
+        $balance = Http::withHeaders($headers)->get(
+                $this->apiUrl . '/wallets/balance/'. $wallet_ref . $this->businessId
+            )->json();
+    }
+
+
+    /**
+     * Account lookup
+     */
+    public function verifyAccount(array $data)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->apiKey,
+            'Content-Type' => 'application/json'
+        ];
+
+        $data = array_merge($data, ['business_id' => $this->businessId]);
+        $balance = Http::withHeaders($headers)->post(
+                $this->apiUrl . '/merchants/utilities/banks/verify',
+                $data
+            )->json();
+
+        return $balance;
+    }
+
+
+    /**
+     * Initiate Payout/Transfer
+     */
+    public function initiateTransfer(array $data)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->apiKey,
+            'Content-Type' => 'application/json'
+        ];
+
+        $data = array_merge($data, ['business_id' => $this->businessId]);
+        $transfer = Http::withHeaders($headers)->post(
+                $this->apiUrl . '/wallets/transfer',
+                $data
+            )->json();
+
+        return $transfer;
+    }
+
+
+    /**
+     * Get all transactions
+     */
+    public function getTransactions(array $data)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->apiKey,
+            'Content-Type' => 'application/json'
+        ];
+        $data = array_merge($data, ['business_id' => $this->businessId]);
+        $balance = Http::withHeaders($headers)->get(
+            $this->apiUrl . '/wallets/transactions?'. http_build_query($data)
+        )->json();
+
+        return $balance;
+    }
+
+
+    /**
+     * Get details of a transaction
+     */
+    public function transactionDetails(string $tx_ref)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->apiKey,
+            'Content-Type' => 'application/json'
+        ];
+        $tx_details = Http::withHeaders($headers)->get(
+            $this->apiUrl . '/wallets/transactions/:' . $tx_ref
+        )->json();
+
+        return $tx_details;
+    }
+
+
+    /**
+     * Retry transfer
+     */
+    public function retryTransfer($tx_ref)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . $this->apiKey,
+            'Content-Type' => 'application/json'
+        ];
+        $transfer = Http::withHeaders($headers)->post(
+            $this->apiUrl . '/wallets/retry-transfer/' . $tx_ref
+        )->json();
+
+        return $transfer;
+    }
 }
